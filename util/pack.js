f7de7080bd2692b5a4fe251d27e60ad04d6e43e3
Q. Zhong <12ff54e@gmail.com>
2026-07-23 16:26:16 +0800

    Move plotly to libs folder and update pack.js

--- util/pack.js
+++ util/pack.js
@@ -4,7 +4,15 @@ const path = require('path');
 
 const packName = 'GTC-Visualization.tar.gz';
 const fileList = [
-    'public',
+    'public/fonts',
+    'public/input',
+    'public/javascripts/bundle.js',
+    'public/javascripts/components/gtc-output-picker.js',
+    'public/libs',
+    'public/shader',
+    'public/stylesheets',
+    'public/favicon.ico',
+    'public/loading.gif',
     'views',
     '.env_example',
     'LICENSE',
